ecabdab727103e232488694d840ab6533bfa5b3b
rangai <fibo.112358132134@gmail.com>
2022-05-22 15:25:11 +0900

    test

---
+++ src/main.rs
@@ -0,0 +1,50 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::env;
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let port_key = "PORT";
+    let default_port = 3000;
+    let port = match env::var(port_key) {
+        Ok(val) => match val.parse::<u16>() {
+            Ok(port) => port,
+            Err(_) => {
+                println!(
+                    "the port number \"{}\" is invalid. default port will be used.",
+                    val
+                );
+                default_port
+            }
+        },
+        Err(_) => {
+            println!(
+                "\"{}\" is not defined in environment variables. default port will be used.",
+                port_key
+            );
+            default_port
+        }
+    };
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("0.0.0.0", port))?
+    .run()
+    .await
+}
